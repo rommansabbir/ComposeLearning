624198b2896f027a6bfd4f029e79b247863d8a75
Romman Sabbir <rommansabbir@gmail.com>
2022-11-04 12:05:17 +0600

    init commit

---
+++ app/src/main/java/com/rommansabbir/composelearning/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.rommansabbir.composelearning
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rommansabbir.composelearning.ui.theme.ComposeLearningTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLearningTheme {
+                // A surface container using the 'background' color from the theme
+                EntryPoint()
+            }
+        }
+    }
+}
+
+@Composable
+fun EntryPoint(){
+    Surface(
+        modifier = Modifier.fillMaxSize().fillMaxHeight().fillMaxWidth(),
+        color = MaterialTheme.colors.background
+    ) {
+        Greeting("Android")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeLearningTheme {
+        EntryPoint()
+    }
+}
