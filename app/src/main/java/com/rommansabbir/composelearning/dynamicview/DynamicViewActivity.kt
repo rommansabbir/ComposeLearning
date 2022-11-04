0b785f11fa4f1818ce86c461be051b358d65a4d5
Romman Sabbir <rommansabbir@gmail.com>
2022-11-04 15:55:44 +0600

    working on dynamic view 1

--- app/src/main/java/com/rommansabbir/composelearning/dynamicview/DynamicViewActivity.kt
+++ app/src/main/java/com/rommansabbir/composelearning/dynamicview/DynamicViewActivity.kt
@@ -3,18 +3,58 @@ package com.rommansabbir.composelearning.dynamicview
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
 import com.rommansabbir.composelearning.ui.theme.ComposeLearningTheme
 
 class DynamicViewActivity : ComponentActivity() {
+    object Models {
+        sealed class DynamicModelID {
+            object BASIC : DynamicModelID()
+            object JOB_INFO : DynamicModelID()
+            object SKILL_SET : DynamicModelID()
+            object WEB_LINKS : DynamicModelID()
+        }
+
+        open class BaseDynamicModel(val id: DynamicModelID)
+        data class DynamicViewModel1(
+            val title: String,
+            val description: String,
+            val image: String
+        ) : BaseDynamicModel(DynamicModelID.BASIC)
+
+        data class DynamicViewModel2(val jobTitle: String, val currentCompany: String) :
+            BaseDynamicModel(
+                DynamicModelID.JOB_INFO
+            )
+
+        data class DynamicViewModel3(val skillsSet: MutableList<String>, val bio: String) :
+            BaseDynamicModel(
+                DynamicModelID.SKILL_SET
+            )
+
+        data class DynamicViewModel4(
+            val linkedInProfile: String,
+            val githubProfile: String,
+            val website: String
+        ) : BaseDynamicModel(DynamicModelID.WEB_LINKS)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,19 +75,49 @@ private fun EntryView() {
             .fillMaxHeight(),
         color = MaterialTheme.colors.background
     ) {
-        Greeting3("Android")
+        DynamicView1(
+            model = getDynamicViewMockedData()[0] as DynamicViewActivity.Models.DynamicViewModel1
+        )
     }
 }
 
 @Composable
-fun Greeting3(name: String) {
-    Text(text = "Hello $name!")
+fun DynamicView1(model: DynamicViewActivity.Models.DynamicViewModel1) {
+    Card(shape = RoundedCornerShape(10.dp)) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(model = model.image),
+                contentDescription = null,
+                modifier = Modifier.size(128.dp)
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text = model.title,
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Normal,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                text = model.description,
+                fontSize = 12.sp,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.Justify
+            )
+            Spacer(modifier = Modifier.size(16.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview3() {
     ComposeLearningTheme {
-        Greeting3("Android")
+        EntryView()
     }
 }
